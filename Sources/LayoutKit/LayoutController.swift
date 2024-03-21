c590749815e2da214ecf6051cd3981cef0267b49
Nikolay Kazanin <artchemist@yandex.ru>
2024-03-21 20:16:05 +0300

    add base Controller

---
+++ Sources/LayoutKit/LayoutController.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+open class Controller: UIViewController {
+    open var contentView: UIView {
+        UIView()
+    }
+    
+    private var _contentView: UIView?
+    
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    public required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        let contentView = contentView
+        _contentView = contentView
+        view.addSubview(contentView)
+    }
+    
+    open override func viewDidLayoutSubviews() {
+        let safeAreaFrame = view.safeAreaLayoutGuide.layoutFrame
+        let contentViewSize = _contentView?.sizeThatFits(
+            safeAreaFrame.size
+        )
+        
+        _contentView?.frame.size = contentViewSize ?? .zero
+        _contentView?.center = CGPoint(x: safeAreaFrame.midX, y: safeAreaFrame.midY)
+    }
+}
