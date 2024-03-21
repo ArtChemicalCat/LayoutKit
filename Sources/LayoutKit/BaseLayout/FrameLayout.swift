1e8759c53c080a7e6d3e6215c270a9504b223044
Nikolay Kazanin <artchemist@yandex.ru>
2024-03-22 00:41:29 +0300

    add min and max dimentions to frame layout

--- Sources/LayoutKit/BaseLayout/FrameLayout.swift
+++ Sources/LayoutKit/BaseLayout/FrameLayout.swift
@@ -3,14 +3,56 @@ import UIKit
 struct FrameLayout: Layout {
     let width: CGFloat?
     let height: CGFloat?
+    let minWidth: CGFloat?
+    let minHeight: CGFloat?
+    let maxWidth: CGFloat?
+    let maxHeight: CGFloat?
+    
     let alignment: Alignment
+    
+    init(width: CGFloat?, height: CGFloat?, alignment: Alignment) {
+        self.width = width
+        self.height = height
+        self.minWidth = nil
+        self.minHeight = nil
+        self.maxWidth = nil
+        self.maxHeight = nil
+        self.alignment = alignment
+    }
+    
+    init(
+        minWidth: CGFloat? = nil,
+        minHeight: CGFloat? = nil,
+        maxWidth: CGFloat? = nil,
+        maxHeight: CGFloat? = nil,
+        alignment: Alignment
+    ) {
+        self.width = nil
+        self.height = nil
+        self.minWidth = minWidth
+        self.minHeight = minHeight
+        self.maxWidth = maxWidth
+        self.maxHeight = maxHeight
+        self.alignment = alignment
+    }
     
     func sizeThatFit(size: CGSize, subviews: [UIView]) -> CGSize {
         guard let view = subviews.first else { return .zero }
-        let size = view.sizeThatFits(size)
-        let height = height ?? size.height
-        let width = width ?? size.width
-        return CGSize(width: width, height: height)
+        
+        if height != nil || width != nil {
+            return view.sizeThatFits(
+                CGSize(
+                    width: width ?? size.width,
+                    height: height ?? size.height
+                )
+            )
+        }
+        
+        let viewSize = view.sizeThatFits(
+            allowedSize(from: size)
+        )
+        
+        return allowedSize(from: viewSize)
     }
     
     func placeSubviews(_ subviews: [UIView], in rect: CGRect) {
@@ -40,11 +82,36 @@ struct FrameLayout: Layout {
             size: size
         )
     }
+    
+    private func allowedSize(from size: CGSize) -> CGSize {
+        CGSize(
+            width: max(min(maxWidth ?? .infinity, size.width), minWidth ?? .zero),
+            height: max(min(maxHeight ?? .infinity, size.height), minHeight ?? .zero)
+        )
+    }
 }
 
 public extension UIView {
     func frame(width: CGFloat? = nil, height: CGFloat? = nil, alignment: Alignment = .center) -> UIView {
         FrameLayout(width: width, height: height, alignment: alignment)(priority: .max) {
+            self
+        }
+    }
+    
+    func frame(
+        minWidth: CGFloat? = nil,
+        minHeight: CGFloat? = nil,
+        maxWidth: CGFloat? = nil,
+        maxHeight: CGFloat? = nil,
+        alignment: Alignment = .center
+    ) -> UIView {
+        FrameLayout(
+            minWidth: minWidth,
+            minHeight: minHeight,
+            maxWidth: maxWidth,
+            maxHeight: maxHeight,
+            alignment: alignment
+        )(priority: .max) {
             self
         }
     }
