c44663466ba09710bcc5257adda4c29ec4521e55
Nikolay Kazanin <artchemist@yandex.ru>
2024-03-21 20:03:19 +0300

    add callAsFunction extension to layout

--- Sources/LayoutKit/Layout.swift
+++ Sources/LayoutKit/Layout.swift
@@ -4,3 +4,11 @@ public protocol Layout {
     func sizeThatFit(size: CGSize, subviews: [UIView]) -> CGSize
     func placeSubviews(_ subviews: [UIView], in rect: CGRect)
 }
+
+extension Layout {
+    func callAsFunction(_ content: () -> UIView) -> UIView {
+        let layoutView = LayoutView(self)
+        layoutView.addSubview(content())
+        return layoutView
+    }
+}
