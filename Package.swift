5b627b3fd62f9db6d86f29410cf82b7890bffeb5
Nikolay Kazanin <artchemist@yandex.ru>
2024-03-22 21:39:26 +0300

    add DeclarativeUIKit dependency

--- Package.swift
+++ Package.swift
@@ -12,11 +12,18 @@ let package = Package(
             name: "LayoutKit",
             targets: ["LayoutKit"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/ArtChemicalCat/DeclarativeUIKit.git", branch: "main")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "LayoutKit"),
+            name: "LayoutKit",
+            dependencies: [
+                .product(name: "DeclarativeUIKit", package: "DeclarativeUIKit")
+            ]
+        ),
         .testTarget(
             name: "LayoutKitTests",
             dependencies: ["LayoutKit"]),
